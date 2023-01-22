2e3daf922b8d44e831e8be507dc89664e0591bc2
ViktorijaVer <viktorija.verbickaite@gmail.com>
2023-01-23 00:04:30 +0200

    feat: src folder was made

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,25 @@
-module.export = {
-    env: {
-      browser: true,
-      es2021: true,
-    },
-    extends: [
-      'airbnb-base',
-      'airbnb-typescript/base',
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      project: './tsconfig.json'
-    },
-    plugins: [
-      '@typescript-eslint',
-    ],
-    rules: {
-      "comma-dangle": "off",
-      "@typescript-eslint/comma-dangle": [2],
-      "@typescript-eslint/indent": [0]
-    },
-  };
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    "comma-dangle": "off",
+    "@typescript-eslint/comma-dangle": [2],
+    'react/jsx-filename-extension': [0],
+    "@typescript-eslint/indent": [0]
+  },
+};
